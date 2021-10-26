b5728c9b01a7491e879fddbde7259a4f5aa514a6
Koji Murata <malt.koji@gmail.com>
2021-10-26 18:19:52 +0900

    test

--- src/aggregator.spec.ts
+++ src/aggregator.spec.ts
@@ -25,4 +25,17 @@ describe("aggregate", () => {
     expect(mocks.datastoreMock.transactionMock.get).toBeCalledWith(key);
     expect(mocks.datastoreMock.transactionMock.upsert).toBeCalledWith({ key, data: { x: 9, y: 18, z: 4 } });
   });
+
+  context("If the actual entity already exists", () => {
+    beforeEach(() => {
+      mocks.datastoreMock.transactionMock.get.mockResolvedValue([{ foo: "bar", x: 10000 }]);
+    });
+
+    it("reflects the value in the actual entity.", async () => {
+      const key = mocks.datastore.key({ path: ["Counter", "dummy-id"] });
+      await aggregate(key);
+
+      expect(mocks.datastoreMock.transactionMock.upsert).toBeCalledWith({ key, data: { foo: "bar", x: 9, y: 18, z: 4 } });
+    });
+  });
 });
