5742b50b53aa8deafc37baf6825ea9be3febe865
Koji Murata <malt.koji@gmail.com>
2021-10-26 22:31:51 +0900

    coverage

--- jest.config.js
+++ jest.config.js
@@ -92,7 +92,7 @@ module.exports = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: "ts-jest",
+  preset: "ts-jest",
 
   // Run tests from one or more projects
   // projects: null,
@@ -169,7 +169,7 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: { "^.+\\.tsx?$": "@mish-tv/esbuild-jest" },
+  // transform: { "^.+\\.tsx?$": "@mish-tv/esbuild-jest" },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
